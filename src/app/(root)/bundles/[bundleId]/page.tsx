27c5ba5907a35c4b3aa8dcd601677dec4b90287e
zironpro <dev@zironpro.ae>
2026-06-05 23:36:14 +0400

    fix: using useTransition on a server component

--- src/app/(root)/bundles/[bundleId]/page.tsx
+++ src/app/(root)/bundles/[bundleId]/page.tsx
@@ -4,14 +4,11 @@ import { Button } from "@/components/ui/button";
 
 import { getWixClient } from "@/lib/wix";
 import { BOOK_BUNDLES_COLLECTION } from "@/lib/wix/constants";
-import { useTransition } from "react";
-import { currentCart } from "@wix/ecom";
 
 export default async function BundleDetailPage(
 	props: PageProps<"/bundles/[bundleId]">
 ) {
 	const { params } = props;
-    const [isPending, startTransition] = useTransition()
 
 	const { bundleId } = await params;
 	const wixClient = (await getWixClient()).use(items);
@@ -25,17 +22,10 @@ export default async function BundleDetailPage(
 
 	const [product] = item.items;
 
-    function handleAddToCart() {
-        startTransition(async () => {
-            const {addToCurrentCart} = (await getWixClient()).use(currentCart); 
-            const {cart} = await addToCurrentCart() 
-        })
-    }
-
 	return (
 		<div className="container">
 			<h1>{product.title}</h1>
-			<Button onClick={handleAddToCart} disabled={isPending}>Add to cart</Button>
+			<Button>Add to cart</Button>
 		</div>
 	);
 }
